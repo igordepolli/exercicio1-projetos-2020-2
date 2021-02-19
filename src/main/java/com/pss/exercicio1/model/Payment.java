8536c1a37eda4d7f4c671fd61135b3b63c8fb043
Igor Depolli <igorcler@gmail.com>
2021-02-19 14:20:43 -0300

    Payment without code

--- src/main/java/com/pss/exercicio1/model/Payment.java
+++ src/main/java/com/pss/exercicio1/model/Payment.java
@@ -2,7 +2,6 @@ package com.pss.exercicio1.model;
 
 public class Payment {
 
-    private int code;
     private Employee employee;
     private Double value;
 
@@ -12,14 +11,6 @@ public class Payment {
     public Payment(Employee employee, Double value) {
         this.employee = employee;
         this.value = value;
-    }
-
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
     }
 
     public Employee getEmployee() {
@@ -36,31 +27,6 @@ public class Payment {
 
     public void setValue(Double value) {
         this.value = value;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 41 * hash + this.code;
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Payment other = (Payment) obj;
-        if (this.code != other.code) {
-            return false;
-        }
-        return true;
     }
 
     public boolean checkPayment() throws Exception {
